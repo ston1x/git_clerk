71f4cd365077815e1bae72048e45d4045a09a7f8
ston1x <stoianovnk@gmail.com>
2020-02-19 23:10:00 +0200

    👨🏽‍💼 ✂️ Cut the paths, append git branch

--- git-clerk.sh
+++ git-clerk.sh
@@ -1,6 +1,25 @@
+paths=()
+statuses=()
 for d in */ ; do
-    cd $d
-    echo $(pwd; git branch --show-current)
+    cd $d;
+    if [[ ! -d .git/ ]]
+    then
+      cd ..
+      continue
+    fi
+    paths+=("$(pwd)")
+    statuses+=("$(git branch --show-current)")
     cd ../
+done;
+# echo "${statuses[*]}"
+# echo "${paths[*]}"
+
+len=${#paths[@]}
+for (( i=0; i<$len; i++ )); do
+  echo "$(echo "...${paths[$i]}" | tail -c 30) ${statuses[$i]}"
 done
 
+
+## Use bash for loop
+# for (( i=0; i<$len; i++ )); do echo "${paths[$i]}" ; done
+# echo $len
