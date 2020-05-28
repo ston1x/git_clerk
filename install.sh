e7d1245f9c2c907a0f024908b712efdf8125a0ec
ston1x <stoianovnk@gmail.com>
2020-05-28 19:43:05 +0300

    🌍 Download the script right from install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,9 @@
 
 # TODO check available directories and privileges
 # it works perfectly for macOS w/ homebrew but ubuntu server needs more privileges
+echo "Downloading the executable"
+curl -o git-clerk.sh https://raw.githubusercontent.com/ston1x/git-clerk/master/git-clerk.sh
+echo "Copying to /usr/local/bin/git-clerk"
 cp git-clerk.sh /usr/local/bin/git-clerk
+echo "Making it executable"
 chmod +x /usr/local/bin/git-clerk
